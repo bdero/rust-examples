d2e220e6cde2086fc6c25669b94aea891408dc4f
Brandon DeRosier <bdero@mit.edu>
2015-02-21 19:57:59 -0500

    7.5.1.3 pointers/ref

---
+++ rust_by_example/7.5.1.3_pointers_ref/pointers_ref.rs
@@ -0,0 +1,24 @@
+fn main() {
+    let reference = &4;
+
+    match reference {
+        &val => println!("Got a value via destructuring: {:?}", val),
+    }
+
+    let _not_a_reference = 3;
+    let ref _is_a_reference = 3;
+
+    let value = 5;
+    let mut mut_value = 6;
+
+    match value {
+        ref r => println!("Got a reference to a value: {:?}", r),
+    }
+
+    match mut_value {
+        ref mut m => {
+            *m += 10;
+            println!("Added 10. New value: {:?}", m);
+        }
+    }
+}
