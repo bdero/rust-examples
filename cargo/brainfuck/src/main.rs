b304a404eefcc26af49dfbd967fa89157f18cbca
Brandon DeRosier <bdero@mit.edu>
2015-03-23 01:50:54 -0400

    Every bf command except for input

--- cargo/brainfuck/src/main.rs
+++ cargo/brainfuck/src/main.rs
@@ -2,6 +2,7 @@ use std::env;
 
 use std::io::prelude::*;
 use std::fs::File;
+use std::char;
 
 
 fn main() {
@@ -30,26 +31,48 @@ fn main() {
     file.ok().unwrap().read_to_string(&mut bf);
 
     // Initialize the tape
-    let mut tape: Box<[u8]> = Box::new([0; 30000]);
+    let mut tape: Box<[u32]> = Box::new([0; 30000]);
 
     // Interpret the contents
     brainfuck(bf.as_bytes(), &mut tape);
 }
 
-fn brainfuck(bf: &[u8], tape: &mut Box<[u8]>) {
+fn brainfuck(bf: &[u8], tape: &mut Box<[u32]>) {
+    let mut loop_stack = Vec::new();
+
     let mut cursor = 0;
     let mut i = 0;
     while i < bf.len() {
-        match (bf[i]) {
+        match bf[i] {
             62 /* > */ => cursor += 1,
             60 /* < */ => cursor -= 1,
             43 /* + */ => tape[cursor] += 1,
             45 /* - */ => tape[cursor] -= 1,
-            46 /* . */ => print!("{}", tape[cursor]),
+            46 /* . */ =>
+                print!("{}",
+                       match char::from_u32(tape[cursor]) {
+                           Some(out) => out,
+                           None => ' ',
+                       }),
             44 /* , */ => println!("in"),
-            91 /* [ */ => println!("begin"),
-            93 /* ] */ => println!("end"),
-            _ => println!("ignore"),
+            91 /* [ */ =>
+                match tape[cursor] {
+                    0 =>
+                        while tape[cursor] != 93 {
+                            cursor += 1;
+                        },
+                    _ => loop_stack.push(i),
+                },
+            93 /* ] */ =>
+                match tape[cursor] {
+                    0 => { loop_stack.pop(); },
+                    _ => i = match loop_stack.last() {
+                        Some(i) => *i,
+                        None =>
+                            panic!("Mismatching loop brackets! Char: {}", i),
+                    },
+                },
+            _ => (),
         }
         i += 1;
     }
